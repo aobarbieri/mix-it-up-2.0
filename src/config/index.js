84e960d5d04e03b12f3258a4631025d6fbc9c023
aobarbieri <aobarbieri@gmail.com>
2023-12-18 14:10:25 -0500

    Move API_KEY inside .env to avoid exposure. Replace API_KEY for security purposes

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
-const { VITE_BASE_URL } = import.meta.env
+
+const { VITE_BASE_URL, VITE_API_KEY } = import.meta.env
 
 export default {
 	BASE_URL: VITE_BASE_URL,
-	API_KEY: '5KSSa0JkDFiHOVd4MJImyw==ynVLwcJyxR8emTSC',
+	API_KEY: VITE_API_KEY,
 }
