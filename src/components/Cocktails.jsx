682d2aafa06d3fe45242422125f777fe124a65da
aobarbieri <aobarbieri@gmail.com>
2023-12-19 18:59:46 -0500

    Complete styles for Your ingredients section

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -20,36 +20,38 @@ export default function Cocktails() {
 					Select which ingredients you have, and we&#39;ll show you all the different cocktails you can make.
 				</p>
 			</section>
-			<section className='app-default-width bg-light-grey mt-5 md:mt-0'>
+
+			<section className='app-default-width mt-5 md:mt-0'>
 				<SearchIngredients findCocktails={handleRequest} />
 
-				<h2>Cocktails you can make</h2>
-				{/* Apply conditional visibility here */}
-				<p>There are no recipes based on the ingredients you selected.</p>
+				<div className='bg-light-grey'>
+					<h2>Cocktails you can make</h2>
+					{/* Apply conditional visibility here */}
+					<p>There are no recipes based on the ingredients you selected.</p>
 
-				<section className='flex flex-wrap justify-between'>
-					{/* Only loop if there are results from the api */}
-					{cocktails.map((c) => (
-						<Link to={`/cocktail/${c.idDrink}`} key={c.idDrink}>
-							<div>
-								<img className='cocktail-thumbnail' src={c.strDrinkThumb} alt='Drink picture' />
+					<section className='flex flex-wrap justify-between'>
+						{/* Only loop if there are results from the api */}
+						{cocktails.map((c) => (
+							<Link to={`/cocktail/${c.idDrink}`} key={c.idDrink}>
+								<div>
+									<img className='cocktail-thumbnail' src={c.strDrinkThumb} alt='Drink picture' />
 
-								<div className='flex justify-between'>
-									<h3>{c.strDrink}</h3>
-									{/* calculate preparation time based on the number of ingredients */}
-									<p>2min</p>
+									<div className='flex justify-between'>
+										<h3>{c.strDrink}</h3>
+										{/* calculate preparation time based on the number of ingredients */}
+										<p>2min</p>
+									</div>
+									{/* List of characteristics - this info comes from a different api call*/}
+									<ul className='flex flex-wrap gap-x-1.5'>
+										<li>strCategory</li>
+										<li>strGlass</li>
+										<li>strAlcoholic</li>
+									</ul>
 								</div>
-
-								{/* List of characteristics - this info comes from a different api call*/}
-								<ul className='flex flex-wrap gap-x-1.5'>
-									<li>strCategory</li>
-									<li>strGlass</li>
-									<li>strAlcoholic</li>
-								</ul>
-							</div>
-						</Link>
-					))}
-				</section>
+							</Link>
+						))}
+					</section>
+				</div>
 			</section>
 		</>
 	)
--- src/components/SearchIngredients.jsx
+++ src/components/SearchIngredients.jsx
@@ -22,14 +22,14 @@ export default function SearchIngredients({ findCocktails }) {
 	}
 
 	return (
-		<section className='bg-white p-5 flex flex-wrap gap-y-5 shadow-sm border-b-1 border-zinc-300'>
+		<section className='bg-white p-5 pb-10 md:p-10 flex flex-wrap gap-y-5 shadow-md border-b border-zinc-300 rounded-t md:rounded-none'>
 			<div className='flex items-center gap-x-3.5'>
 				<img src={ingredientsIcon} alt='Ingredients' />
-				<h2 className='font-bold text-xl leading-7'>Your ingredients</h2>
+				<h2 className='font-bold text-xl md:text-2xl leading-7'>Your ingredients</h2>
 			</div>
 			<form onSubmit={handleSubmit}>
 				<div className='relative'>
-					<input className='rounded-input' type='search' placeholder='Search ingredients' name='search' onChange={handleChange} />
+					<input className='rounded-input md:w-1/2' type='search' placeholder='Search ingredients' name='search' onChange={handleChange} />
 					<button className='absolute w-6 top-2.5 left-3 z-10' type='submit'>
 						<img src={searchIcon} alt='search icon' />
 					</button>
