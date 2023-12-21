44d0828c3c467658c01df77a3b8c4fb096010fe5
aobarbieri <aobarbieri@gmail.com>
2023-12-21 12:56:28 -0500

    Update footer app description

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,8 @@ export default function Footer() {
 			<section className='app-default-width h-full flex flex-col md:flex-row md:max-w-xl justify-end items-center pb-10'>
 				<img className='w-24 md:mr-2.5' src={logo} alt='' />
 				<p className='text-white text-sm'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, sed esse aliquid dignissimos repellendus pariatur eum vitae cum quos hic.
+					Your ultimate source for exciting drink recipes! Discover a world of creative cocktails and refreshing beverages with easy-to-follow
+					instructions. Elevate your mixology game and impress your guests at any gathering. Cheers to endless flavor possibilities!
 				</p>
 			</section>
 		</footer>
