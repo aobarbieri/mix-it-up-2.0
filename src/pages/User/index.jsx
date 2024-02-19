6ef7090d9534316644e93f7baa5bef9d12be64a0
aobarbieri <aobarbieri@gmail.com>
2024-02-19 18:05:06 -0500

    Finish inital Auth0 setup

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,3 +1,25 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { Navigate } from 'react-router'
+
 export default function Profile() {
-	return <div>Authenticated</div>
+	const { user, isAuthenticated, isLoading } = useAuth0()
+
+	if (!isAuthenticated) {
+		return <Navigate to='/' />
+	}
+
+	if (isLoading) {
+		return <div>Loading ...</div>
+	}
+	return (
+		isAuthenticated && (
+			<>
+				<img src={user.picture} alt={user.name} />
+				<h2>{user.name}</h2>
+				<p>{user.email}</p>
+			</>
+		)
+	)
 }
+
+//res.locals for full-stack?
