21d14171449956a64c03724b5a2e696e4ec671c2
aobarbieri <aobarbieri@gmail.com>
2024-02-29 14:24:54 -0500

    Remove Auth0

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,7 +3,6 @@ import Cocktails from '../../pages/Cocktails'
 import Show from '../../pages/Show'
 import Recipes from '../../pages/Recipes'
 import Favorites from '../../pages/Favorites'
-import Profile from '../../pages/User'
 
 export default function Main() {
 	return (
@@ -13,7 +12,6 @@ export default function Main() {
 				<Route path='/cocktail/:id' element={<Show />} />
 				<Route path='/recipes' element={<Recipes />} />
 				<Route path='/favorites' element={<Favorites />} />
-				<Route path='/profile' element={<Profile />} />
 			</Routes>
 		</main>
 	)
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
-import Auth0ProviderWithNavigate from './components/Auth/AuthProvider.jsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<Router>
-		<Auth0ProviderWithNavigate>
-			<App />
-		</Auth0ProviderWithNavigate>
+		<App />
 	</Router>
 )
