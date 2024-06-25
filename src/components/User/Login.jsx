7d2068552eebdaf7f85563ad3785ba1f1a31f328
aobarbieri <aobarbieri@gmail.com>
2024-06-25 17:02:05 -0400

    Create css files components that will hold specific properties for different html elements' group

--- src/components/User/Login.jsx
+++ src/components/User/Login.jsx
@@ -1,45 +1,61 @@
-import { useState } from 'react'
-import { login } from '../../utilities/user-service'
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+import { login } from "../../utilities/user-service";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-	const [user, setUser] = useState({
-		email: '',
-		password: '',
-	})
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-	function handleChange(e) {
-		setUser({ ...user, [e.target.name]: e.target.value })
-	}
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
 
-	async function handleSubmit(e) {
-		e.preventDefault()
-		const userData = await login(user)
-		console.log(userData)
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const userData = await login(user);
+    console.log(userData);
 
-		setUser({
-			email: '',
-			password: '',
-		})
-	}
+    setUser({
+      email: "",
+      password: "",
+    });
+  }
 
-	return (
-		<>
-			<h1>Login into your account</h1>
+  return (
+    <div className="user-container flex-center">
+      <section className="user-content">
+        <h1>Login into Mix It Up</h1>
 
-			<form onSubmit={handleSubmit}>
-				<label htmlFor='email'>Email</label>
-				<input type='text' id='email' name='email' value={user.email} placeholder='email' onChange={handleChange} />
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={user.email}
+            placeholder="Email"
+            onChange={handleChange}
+          />
 
-				<label htmlFor='password'>password</label>
-				<input type='password' id='password' name='password' value={user.password} onChange={handleChange} />
+          <label htmlFor="password">password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={user.password}
+            placeholder="Password"
+            onChange={handleChange}
+          />
 
-				<input type='submit' value='Login' />
-			</form>
+          <input className="btn-primary" type="submit" value="Log In" />
+        </form>
 
-			<p>
-				New here? <Link to='/signup'>Create account</Link>
-			</p>
-		</>
-	)
+        <p>
+          New here? <Link to="/signup">Create new account</Link>
+        </p>
+      </section>
+    </div>
+  );
 }
